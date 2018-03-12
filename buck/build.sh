f457198542e36b9550584bcf05c29009468e4ca7
Geoffrey Hunter <gbmhunter@gmail.com>
2018-03-12 10:29:11 -0700

    CMake library is now being packaged and can be used with find_package().

---
+++ buck/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+buck run :Demo
--- cmake/build.sh
+++ cmake/build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+# Build/install TestLib
+cd lib/TestLib
 mkdir -p build
 cd build
-cmake ..
+cmake -DCMAKE_INSTALL_PREFIX=../install ..
+make install
+
+cd ../../..
+mkdir -p build
+cd build
+echo "PWD = ${PWD}"
+cmake -DCMAKE_PREFIX_PATH=lib/TestLib/install/ -DCMAKE_INSTALL_PREFIX=../install ..
 make
-./CMakeExample
+./Demo
